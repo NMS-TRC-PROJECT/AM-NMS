08309ad9c513115996f0f54ed18d7aa035bdfe1b
ssolkim <skim@amuzlab.com>
2022-12-21 10:28:06 +0900

    [TEST] VOD 트랜스코딩 작업상태 업데이트 테스트 추가

--- src/test/java/com/project/study/service/TranscodeServiceTest.java
+++ src/test/java/com/project/study/service/TranscodeServiceTest.java
@@ -1,5 +1,8 @@
 package com.project.study.service;
 
+import com.project.study.dto.WorkStatusRespDto;
+import com.project.study.model.WorkStatus;
+import com.project.study.repository.WorkStatusRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -16,6 +21,9 @@ class TranscodeServiceTest {
     @Autowired
     TranscodeService transcodeService;
 
+    @Autowired
+    WorkStatusRepository workStatusRepository;
+
     @Test
     @DisplayName("vod 작업요청 테스트")
     void requestTrcVod() {
@@ -23,6 +31,20 @@ class TranscodeServiceTest {
 
         // when
         transcodeService.requestTrcVod();
+
+        // then
+
+    }
+
+    @Test
+    @DisplayName("vod 작업상태 업데이트 테스트")
+    void updateStatus(){
+        // given
+        WorkStatus workStatus = workStatusRepository.findByTransactionId("0bdd0854-8c61-4db5-89f7-c66bc3b55f58");
+        workStatus.setStatus(2);
+
+        // when
+        transcodeService.updateVodTrcStatus(workStatus);  //테스트 후에 DTO를 도입함 ㅋㅅㅋ 나는야 몽총잉
 
         // then
 
